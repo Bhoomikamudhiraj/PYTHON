d412e509ff69948cae3951b16e518b392a0bab83
Bhoomikamudhiraj <gajaravinabhoomika@gmail.com>
2026-01-12 11:55:36 +0530

    python

---
+++ slice.py
@@ -0,0 +1,44 @@
+# a = "bhoomika"
+# print(a[3:6])
+
+#slicing at start :
+# q= "hello,i'm bhoomika"
+# print(q [:8])
+
+# slicing at end :
+# e= "python lang"
+# print(e[2:])
+
+# upper-lower case: 
+# y="i'm a  strong girl"
+# print(y.upper())
+
+# y="dance is my happiness"
+# print(y.lower())
+
+# strip(WHITESPACE):
+# a= "hello  world"
+# print(a.strip())
+
+# replace :
+# q="royal challenge"
+# print(q.replace("r","y"))
+
+# spilt :
+# a="hello ,world"
+# print(a.split(","))
+
+# concetenation :
+# a="hi"
+# b="hii"
+# c=a+b
+# print(c)
+
+# to give the space :
+# q= "hello"
+# w="world"
+# print(q + " " + w)
+
+# escape (\) :
+# q= "hello,\"'i'm\" bhoomi"
+# print(q)
