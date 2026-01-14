5f8193787af543fe94fe5a47a7b810e3929f8f62
Bhoomikamudhiraj <gajaravinabhoomika@gmail.com>
2026-01-14 22:50:21 +0530

    python

---
+++ boolean.py
@@ -0,0 +1,66 @@
+# a=19
+# b=12
+# if a>b :
+#     print("greater")
+# else :
+#     print("smaller")  
+
+# x="hello"
+# y=0
+# print(bool(x))
+# print(bool(y))
+
+# arthemetic :
+# q=10
+# w=15
+# print(q+10)
+# print(q*w)
+# print(q%w)
+# print(q-w)
+# print(q/w)
+
+
+# assignment:
+x=14
+x+=5
+print(x)
+# -=,/=,*=,etc:
+
+#comparison :
+# x=2
+# y=3
+# print(x==y)
+# print(x>=y)
+# print(x<=y)
+# print(x>y)
+# print(x<y)
+# print(x!=y)
+
+# logical op:
+# and(return true if both statements are true)
+# or(return true if one of the statements is true )
+# not (reverse the false ,if the result is true)
+
+#identity operator:
+# x="hello"
+# y="hello"
+# z=x
+# print(x is z)
+# #therefore it returns true coz, z is the same object as x
+# print(x is y)
+# print(x==y)
+# print(x is y)
+
+# membership op: 
+# checks whether the value is present in the remaining sequence or not
+# x =("apple","banana")
+# y=("apple","mango")
+# print("apple" in x)
+# in = present,not in(true) = does not exist(false)
+
+# bitwise op:
+# and(&)
+# or(|)
+# not(~)
+# >>,<<
+# xor(^)
