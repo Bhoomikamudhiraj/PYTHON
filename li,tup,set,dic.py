41f189e561012603dab5c140bc2ab160929f1026
Bhoomikamudhiraj <gajaravinabhoomika@gmail.com>
2026-01-17 16:53:02 +0530

    python

---
+++ li,tup,set,dic.py
@@ -0,0 +1,37 @@
+# list:change,ordered,allow duplicates
+# q=['hi','hello','hai','hi']
+# print(q)
+
+# to print the lenght:
+# mylist=['hi','bhoomika']
+# print(len(mylist))
+
+# to know  the index of the items:
+q=['hi','hello','bhoomi']
+print(q[2])
+
+# to change the item:
+# w=["hi","hello"]
+# w[1]="bhoomi"
+# print(w)
+# append:
+# q=['hi']
+# q.append('hello')
+# print(q)
+
+# remove,clear,del,pop
+
+# tuples :
+# similar to list but it is unchangeable
+# q=('hello',)
+# print(type(q))
+
+# set :
+# w={'hi','hello'}
+# print(w)
+
+# dictionaries:
+mydict= { 
+     "hello" : "bhoomi"
+}
+print(mydict)
